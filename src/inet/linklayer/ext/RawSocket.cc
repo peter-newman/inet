bd73eedd55c061e5b49e7937008a2c288e848155
Gyorgy Szaszko <adam.szaszko@gmail.com>
2018-07-02 18:04:40 +0200

    ext: MAC frame sending changes

--- src/inet/linklayer/ext/RawSocket.cc
+++ src/inet/linklayer/ext/RawSocket.cc
@@ -20,6 +20,9 @@
 #include <omnetpp/platdep/sockets.h>
 #include <net/if.h>
 #include <arpa/inet.h>
+#include <netinet/ether.h>
+#include <sys/ioctl.h>
+#include <linux/if_packet.h>
 
 #include "inet/common/INETUtils.h"
 #include "inet/common/ModuleAccess.h"
@@ -44,11 +47,29 @@ void RawSocket::initialize(int stage)
     cSimpleModule::initialize(stage);
     if (stage == INITSTAGE_LOCAL) {
         device = par("device");
-        const char *protocol = par("protocol");
-        if (!strcmp(protocol, "ipv4"))
+        const char *protocolName = par("protocol");
+        if (!strcmp(protocolName, "ipv4")) {
+            protocol = &Protocol::ipv4;
             fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-        else if (!strcmp(protocol, "ethernetMac"))
+        }
+        else if (!strcmp(protocolName, "ethernetMac")) {
+            protocol = &Protocol::ethernetMac;
+            struct ifreq if_mac;
+            struct ifreq if_idx;
             fd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW);
+            /* Get the index of the interface to send on */
+            memset(&if_idx, 0, sizeof(struct ifreq));
+            strncpy(if_idx.ifr_name, device, IFNAMSIZ-1);
+            if (ioctl(fd, SIOCGIFINDEX, &if_idx) < 0)
+                perror("SIOCGIFINDEX");
+            /* Get the MAC address of the interface to send on */
+            memset(&if_mac, 0, sizeof(struct ifreq));
+            strncpy(if_mac.ifr_name, device, IFNAMSIZ-1);
+            if (ioctl(fd, SIOCGIFHWADDR, &if_mac) < 0)
+                perror("SIOCGIFHWADDR");
+            ifindex = if_idx.ifr_ifindex;
+            myMacAddress.setAddressBytes(if_mac.ifr_hwaddr.sa_data);
+        }
         else
             throw cRuntimeError("Unknown protocol");
         if (fd == INVALID_SOCKET)
@@ -76,18 +97,42 @@ void RawSocket::handleMessage(cMessage *msg)
 //    if (protocol != &Protocol::ipv4)
 //        throw cRuntimeError("ExtInterface accepts ipv4 packets only");
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
+    struct sockaddr_in ip_addr;
+    struct sockaddr_ll socket_address;
+    struct sockaddr * addr = nullptr;
+    size_t addrsize = 0;
+
+    if (protocol == &Protocol::ipv4) {
+        ip_addr.sin_family = AF_INET;
 #if !defined(linux) && !defined(__linux) && !defined(_WIN32)
-    addr.sin_len = sizeof(struct sockaddr_in);
+        ip_addr.sin_len = sizeof(struct sockaddr_in);
 #endif // if !defined(linux) && !defined(__linux) && !defined(_WIN32)
-    addr.sin_port = htons(0);
+        ip_addr.sin_port = htons(0);
+        addr = (struct sockaddr *)&ip_addr;
+        addrsize = sizeof(ip_addr);
+    }
+    else if (protocol == &Protocol::ethernetMac) {
+        /* Index of the network device */
+        socket_address.sll_ifindex = ifindex;
+        /* Address length*/
+        socket_address.sll_halen = ETH_ALEN;
+        /* Destination MAC */
+        socket_address.sll_addr[0] = myMacAddress.getAddressByte(0);
+        socket_address.sll_addr[1] = myMacAddress.getAddressByte(1);
+        socket_address.sll_addr[2] = myMacAddress.getAddressByte(2);
+        socket_address.sll_addr[3] = myMacAddress.getAddressByte(3);
+        socket_address.sll_addr[4] = myMacAddress.getAddressByte(4);
+        socket_address.sll_addr[5] = myMacAddress.getAddressByte(5);
+        addr = (struct sockaddr *)&socket_address;
+        addrsize = sizeof(socket_address);
+    }
+
     auto bytesChunk = packet->peekAllAsBytes();
     uint8 buffer[1 << 16];
     size_t packetLength = bytesChunk->copyToBuffer(buffer, sizeof(buffer));
     ASSERT(packetLength == (size_t)packet->getByteLength());
 
-    sendBytes(buffer, packetLength, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+    sendBytes(buffer, packetLength, addr, addrsize);
     numSent++;
     delete packet;
 }
