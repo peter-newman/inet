e20d985ac7cc3b147c8347c5d6d1cc1357dcfec7
Rudolf Hornig <rudi@omnetpp.org>
2018-07-11 17:14:13 +0200

    converter scripts

---
+++ doc/src/developers-guide/tex2rst-postprocess.pl
@@ -0,0 +1,14 @@
+s/^::$//g;
+s/^.. raw:: latex$//g;
+
+s/   ###ned###/.. code-block:: ned\n/g;
+s/   ###note###/.. note::\n/g;
+
+s/:ttt:§(.*?)§/``$1``/g;
+s/:cppclass:§(.*?)§/:cppclass:`$1`/g;
+s/:ffunc:§(.*?)§/:ffunc:`$1`/g;
+s/:fvar:§(.*?)§/:fvar:`$1`/g;
+s/:protocol:§(.*?)§/:protocol:`$1`/g;
+
+s|\s*\\cppsnippet\{(.*?)\}\{(.*?)\}|.. literalinclude:: lib/Snippets.cc\n   :language: cpp\n   :start-after: !$1\n   :end-before: !End\n   :name: $2|g;
+s|\s*\\msgsnippet\{(.*?)\}\{(.*?)\}|.. literalinclude:: lib/Snippets.msg\n   :language: msg\n   :start-after: !$1\n   :end-before: !End\n   :name: $2|g;
---
+++ doc/src/developers-guide/tex2rst-preprocess.pl
@@ -0,0 +1,11 @@
+s/\\begin\{ned\}/\\begin{verbatim}###ned###/g;
+s/\\end\{ned\}/\\end{verbatim}/g;
+
+s/\\begin\{note\}/\\begin{verbatim}###note###/g;
+s/\\end\{note\}/\\end{verbatim}/g;
+
+s/\\ttt\{(.+?)\}/:ttt:§$1§/g;
+s/\\cppclass\{(.+?)\}/:cppclass:§$1§/g;
+s/\\ffunc\{(.+?)\}/:ffunc:§$1§/g;
+s/\\fvar\{(.+?)\}/:fvar:§$1§/g;
+s/\\protocol\{(.+?)\}/:protocol:§$1§/g;
